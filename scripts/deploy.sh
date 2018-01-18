7616e5cae0c76a606691ea4ff3028e4e3028d983
Kai Davenport <kaiyadavenport@gmail.com>
2018-01-18 23:09:01 +0000

    forgot stdin flag

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,7 @@ function deploy-manifest() {
   local filename="$1"
   echo "running manifest: $filename"
   cat "/app/$filename" | envsubst
-  cat "/app/$filename" | envsubst | kubectl apply -f
+  cat "/app/$filename" | envsubst | kubectl apply -f -
 }
 
 deploy-manifest deploy/00-namespace.yaml

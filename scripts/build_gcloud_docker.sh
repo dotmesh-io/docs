cd9d0e662d85596c8c9a9e2101e849de41c9fb32
Kai Davenport <kaiyadavenport@gmail.com>
2018-01-18 23:42:14 +0000

    update the comment for the gcloud image builder

--- scripts/build_gcloud_docker.sh
+++ scripts/build_gcloud_docker.sh
@@ -1,18 +1,6 @@
 #!/bin/bash -e
 
-# run a docker container that has gcloud installed alongside the docker binary
-# invoke the gcloud_auth_wrapper.sh script passing the command we want to run
-#
-# this will authenticate the gcloud cli using the GCLOUD_SERVICE_KEY var
-#
-# it then runs the command passed
-#
-# the build folder is mounted to /app so we can run scripts that are part
-# of the repo
-#
-# this enables us to push images to GCR and manifests to GKE
-#
-# example usage: bash scripts/gcloud_docker.sh bash /app/pushimages.sh 
+# build the docker image that has gcloud and the docker client inside
 #
 # NOTE: this should end up in a shared repo where each deplouyable repo can pull an image to use
 
